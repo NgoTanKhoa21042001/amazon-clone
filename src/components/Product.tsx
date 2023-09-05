931714138a73f70dcd3580323c06e988371e2e81
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-09-05 20:57:59 +0700

    loading product detail

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -53,33 +53,33 @@ const Product = ({ productData }: any) => {
                   height={300}
                   className="w-full h-full object-cover scale-90 hover:scale-100 transition-transform duration-300"
                 />{" "}
-                <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex flex-col group-hover:translate-x-0 duration-300 transition-transform translate-x-20">
-                  <span className="w-full h-full border-b-[1px] text-lg border-b-gray-400 flex items-center justify-center hover:bg-amazon_yellow cursor-pointer duration-300">
-                    <HiShoppingCart />
-                  </span>
-                  <span
-                    onClick={() =>
-                      dispatch(
-                        addToFavorite({
-                          _id: _id,
-                          title: title,
-                          brand: brand,
-                          category: category,
-                          description: description,
-                          image: image,
-                          isNew: isNew,
-                          oldPrice: oldPrice,
-                          price: price,
-                          quantity: 1,
-                        })
-                      )
-                    }
-                    className="w-full h-full border-b-[1px] text-lg border-b-gray-400 flex items-center justify-center hover:bg-amazon_yellow cursor-pointer duration-300"
-                  >
-                    <FaHeart />
-                  </span>
-                </div>
               </Link>
+              <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex flex-col group-hover:translate-x-0 duration-300 transition-transform translate-x-20">
+                <span className="w-full h-full border-b-[1px] text-lg border-b-gray-400 flex items-center justify-center hover:bg-amazon_yellow cursor-pointer duration-300">
+                  <HiShoppingCart />
+                </span>
+                <span
+                  onClick={() =>
+                    dispatch(
+                      addToFavorite({
+                        _id: _id,
+                        title: title,
+                        brand: brand,
+                        category: category,
+                        description: description,
+                        image: image,
+                        isNew: isNew,
+                        oldPrice: oldPrice,
+                        price: price,
+                        quantity: 1,
+                      })
+                    )
+                  }
+                  className="w-full h-full border-b-[1px] text-lg border-b-gray-400 flex items-center justify-center hover:bg-amazon_yellow cursor-pointer duration-300"
+                >
+                  <FaHeart />
+                </span>
+              </div>
               {/* Price */}
               {isNew && (
                 <p className="absolute top-0 right-0 text-amazon_blue font-medium tracking-wide text-xs animate-bounce">
--- src/pages/[_id].tsx
+++ src/pages/[_id].tsx
@@ -5,74 +5,91 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { BeatLoader } from "react-spinners";
 
 const DynamicPage = () => {
   const [product, setProduct] = useState<any>({});
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
     // đi theo cái query tương ứng với product
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
     setProduct(router.query);
   }, [router.query]);
   return (
     <div className="max-w-screen-xl mx-auto px-4 md:py-10">
-      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2 bg-gray-100 rounded-lg">
-        {/* Image */}
-        <div className="flex items-center justify-center bg-gray-200 rounded-lg relative group overflow-hidden">
-          <Image src={product.image} width={500} height={500} />
+      {isLoading ? (
+        <div className="w-full flex flex-col gap-6 items-center justify-center py-20">
+          <p>Your product is loading...</p>
+          <BeatLoader color="#131921" size={40} />
         </div>
-        {/* Image */}
-        {/* Content */}
-        <div className="md:col-span-2 flex flex-col gap-3 justify-center p-4">
-          <p className="text-xs md:text-sm text-amazon_blue font-semibold mb-3">
-            {product.category}_{product.brand}
-          </p>
-          <h1 className="text-xl md:text-3xl tracking-wide font-semibold">
-            {product.title}
-          </h1>
-          <p className="text-sm text-gray-600">{product.description}</p>
-          <div>
-            <p className="text-base text-gray-600 flex items-center gap-1">
-              Price:
-              <span className="text-amazon_blue font-semibold">
-                <FormattedPrice amount={product.price} />
-              </span>
-              <span className="ml-1 line-through">
-                <FormattedPrice amount={product.oldPrice} />
-              </span>
-            </p>
-            <p className="text-sm text-gra flex items-center gap-1">
-              You saved:
-              <span>
-                <FormattedPrice amount={product.oldPrice - product.price} />
-              </span>
-            </p>
+      ) : (
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2 bg-gray-100 rounded-lg">
+          {/* Image */}
+          <div className="flex items-center justify-center bg-gray-200 rounded-lg relative group overflow-hidden">
+            <Image
+              src={product.image}
+              alt="product image"
+              width={500}
+              height={500}
+            />
           </div>
-          <button
-            onClick={() =>
-              dispatch(
-                addtoCart({
-                  _id: product._id,
-                  brand: product.brand,
-                  category: product.category,
-                  description: product.description,
-                  image: product.image,
-                  isNew: product.isNew,
-                  oldPrice: product.oldPrice,
-                  price: product.price,
-                  title: product.title,
-                  quantity: 1,
-                })
-              )
-            }
-            className="w-full md:w-96 h-12 bg-amazon_blue text-gray-200 hover:bg-amazon_yellow hover:text-amazon_blue duration-300 rounded-lg mt-5 text-base font-semibold"
-          >
-            add to cart
-          </button>
+          {/* Image */}
+          {/* Content */}
+          <div className="md:col-span-2 flex flex-col gap-3 justify-center p-4">
+            <p className="text-xs md:text-sm text-amazon_blue font-semibold mb-3">
+              {product.category}_{product.brand}
+            </p>
+            <h1 className="text-xl md:text-3xl tracking-wide font-semibold">
+              {product.title}
+            </h1>
+            <p className="text-sm text-gray-600">{product.description}</p>
+            <div>
+              <p className="text-base text-gray-600 flex items-center gap-1">
+                Price:
+                <span className="text-amazon_blue font-semibold">
+                  <FormattedPrice amount={product.price} />
+                </span>
+                <span className="ml-1 line-through">
+                  <FormattedPrice amount={product.oldPrice} />
+                </span>
+              </p>
+              <p className="text-sm text-gra flex items-center gap-1">
+                You saved:
+                <span>
+                  <FormattedPrice amount={product.oldPrice - product.price} />
+                </span>
+              </p>
+            </div>
+            <button
+              onClick={() =>
+                dispatch(
+                  addtoCart({
+                    _id: product._id,
+                    brand: product.brand,
+                    category: product.category,
+                    description: product.description,
+                    image: product.image,
+                    isNew: product.isNew,
+                    oldPrice: product.oldPrice,
+                    price: product.price,
+                    title: product.title,
+                    quantity: 1,
+                  })
+                )
+              }
+              className="w-full md:w-96 h-12 bg-amazon_blue text-gray-200 hover:bg-amazon_yellow hover:text-amazon_blue duration-300 rounded-lg mt-5 text-base font-semibold"
+            >
+              add to cart
+            </button>
+          </div>
+          {/* Content */}
         </div>
-        {/* Content */}
-      </div>
+      )}
     </div>
   );
 };
